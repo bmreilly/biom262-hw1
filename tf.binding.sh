ddf5badb97dff601fa2333c7f9dbde82dedf94d5
Brian Reilly <reilly.brian.m@gmail.com>
2016-01-12 10:05:09 -0800

    Create tf.binding.sh
    Still need to add scripts for exercises

---
+++ tf.binding.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#PBS -q hotel
+
+#PBS -N 160112.biom262.hw1
+
+#PBS -l nodes=1:ppn=1
+
+#PBS -l walltime=01:00:00
+
+#PBS -o tf.binding.sh.o
+
+#PBS -e tf.binding.sh.e
+
+#PBS -V 
+
+#PBS -M bmreilly@ucsd.edu
+
+#PBS -m abe
+
+#PBS -A bejar-group
+
+# load programs we need 
+module load biotools
+
+# change to directory where we have our data files + we want our output files
+cd ~/biom262/biom262_hw/biom262-hw1
+
+# exercise 1
+
+# exercise 2
+
+# exercise 3
+
+# exercise 4
+
+# exercise 5
+
+
